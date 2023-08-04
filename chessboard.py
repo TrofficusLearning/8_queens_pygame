a6b6810ff80b6f2811d47e1c329516aafe0116f2
MichaelisTrofficus <miguel.otero.pedrido.1993@gmail.com>
2023-08-04 09:20:57 +0200

    Add files for playground 8-queens and for automatically solve the problem (not working)

--- chessboard.py
+++ chessboard.py
@@ -103,3 +103,51 @@ class Chessboard:
                             pygame.draw.rect(self.attacked_squares_overlay, (0, 0, 0, 0),
                                              (c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE))
         screen.blit(self.attacked_squares_overlay, (0, 0))
+
+
+class ChessboardBacktracking:
+    def __init__(self):
+        self.board = [[None] * BOARD_SIZE for _ in range(BOARD_SIZE)]
+        self.sprites = pygame.sprite.Group()
+        self.chessboard_sprite: pygame.sprite.Sprite = ChessboardSprite()
+        self.sprites.add(self.chessboard_sprite)
+        self.attacked_squares_overlay = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.SRCALPHA)
+
+    def get_queen_at(self, row, col):
+        return self.board[row][col]
+
+    def place_queen(self, row, col):
+        if self.board[row][col] is None:
+            piece = Queen(QUEEN_IMAGE, row, col)
+            self.board[row][col] = piece
+            self.sprites.add(piece)
+
+    def remove_queen(self, row, col):
+        piece = self.board[row][col]
+        if piece is not None:
+            self.board[row][col] = None
+            self.sprites.remove(piece)
+            if hasattr(piece, "attacked_squares"):
+                self.attacked_squares_overlay.fill((0, 0, 0, 0))  # Clear attacked squares overlay
+                for r, c in piece.attacked_squares:
+                    if (r, c) != (piece.row, piece.col):
+                        pygame.draw.rect(self.attacked_squares_overlay, (255, 0, 0, 100),
+                                         (c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
+
+    def draw(self, screen):
+        self.sprites.draw(screen)
+
+        # Clear the attacked_squares_overlay
+        self.attacked_squares_overlay.fill((0, 0, 0, 0))
+
+        for piece in self.sprites:
+            if isinstance(piece, Queen):
+                if hasattr(piece, "attacked_squares"):
+                    for r, c in piece.attacked_squares:
+                        if (r, c) != (piece.row, piece.col):
+                            pygame.draw.rect(self.attacked_squares_overlay, (255, 0, 0, 100),
+                                         (c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+                        else:
+                            pygame.draw.rect(self.attacked_squares_overlay, (0, 0, 0, 0),
+                                             (c * CELL_SIZE, r * CELL_SIZE, CELL_SIZE, CELL_SIZE))
+        screen.blit(self.attacked_squares_overlay, (0, 0))
